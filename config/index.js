9a220a89b39ce18a099d1906fe8a9f5912c374c5
hitesh19426 <hitesh19426@iiitd.ac.in>
2023-02-22 01:31:10 +0530

    updated production url

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3001' : 'https://ddinter-main.vercel.app/'
+export const server = dev ? 'https://ddinter-main.vercel.app/' : 'https://ddinter-main.vercel.app/'

fed237ce55f6a553d8f1643c3f17608b9efca400
hitesh19426 <hitesh19426@iiitd.ac.in>
2023-02-22 01:26:21 +0530

    updated production domain

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3001' : 'https:something.com'
+export const server = dev ? 'http://localhost:3001' : 'https://ddinter-main.vercel.app/'

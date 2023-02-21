090e94fcb31244987af85488581ceb849dd6222a
hitesh19426 <hitesh19426@iiitd.ac.in>
2023-02-22 00:54:56 +0530

    connected to mongoDB, added DB, got interactions

--- pages/api/getInteraction.js
+++ pages/api/getInteraction.js
@@ -1,14 +1,40 @@
+import { connectToDatabase } from "@/lib/mongodb";
 
-export default function handler(req, res, next) {
-    console.log('In API ROUTE')
+export default async function handler(req, res, next) {
+  if (req.method === "POST") {
+    console.log("In POST API ROUTE");
+    const { db } = await connectToDatabase();
+    const drugList = req.body.drugList;
+    console.log("drugList = ", drugList);
 
-    if(req.method === 'POST'){
-        const drugList = req.body.drugList;
-        console.log("drugList = ", drugList);
-    
-        const obj = { id: 1, name1: "drug1", name2: "drug2", level: "Major" };
-        return res.status(200).json({interactions: [obj]});    
+    const interactionsList = [];
+    for (var i = 0; i < drugList.length; i++) {
+      for (var j = 0; j < drugList.length; j++) {
+        const interaction = await db
+          .collection("interactions")
+          .find({ Drug_A: drugList[i]["name"], Drug_B: drugList[j]["name"] })
+          .toArray();
+        if (interaction.length !== 0) {
+          interactionsList.push(interaction[0]);
+        }
+      }
     }
 
-    return res.status(400).json({message: "This route is not defined"});
+    const level_to_int = {
+      Major: 0,
+      Moderate: 1,
+      Minor: 2,
+      Unknown: 3,
+    };
+    
+    interactionsList.sort(
+      (a, b) => level_to_int[a["Level"]] - level_to_int[b["Level"]]
+    );
+
+    console.log(interactionsList);
+
+    return res.status(200).json({ interactions: interactionsList });
+  }
+
+  return res.status(400).json({ message: "This route is not defined" });
 }
